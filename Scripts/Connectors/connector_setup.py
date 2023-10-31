b76e8f4054d24d765e752ffbeb84992d699c9101
jhammercb <john@cloudbrink.io>
2023-10-30 20:30:59 -0400

    It's Alive!

---
+++ Scripts/Connectors/connector_setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+import hashlib
+
+def download_tar_file():
+    print("Downloading the tar file...")
+    cmd = [
+        "sudo", "curl", "--user", "cbuser210:CBUSR#1729", 
+        "-o", "/root/config_connector-12.2.6_1.tar.gz", 
+        "https://d.cloudbrink.com/common/config_connector-12.2.6_1.tar.gz"
+    ]
+    subprocess.run(cmd, check=True)
+
+def verify_md5sum():
+    print("Verifying the MD5 checksum...")
+    cmd = ["sudo", "md5sum", "/root/config_connector-12.2.6_1.tar.gz"]
+    result = subprocess.run(cmd, capture_output=True, text=True)
+    checksum = result.stdout.split()[0]
+    return checksum == "b1189aa7369734e47413a3f4cea972ec"
+
+def extract_files():
+    print("Extracting the files...")
+    cmd = [
+        "sudo", "tar", "-xvf", "/root/config_connector-12.2.6_1.tar.gz"
+    ]
+    subprocess.run(cmd, check=True)
+
+def execute_script():
+    print("Executing the configure_node.sh script...")
+    os.chdir("config_connector-12.2.6")
+    cmd = ["sudo", "bash", "configure_node.sh"]
+    subprocess.run(cmd, check=True)
+
+def main():
+    download_tar_file()
+    if not verify_md5sum():
+        print("MD5 checksum verification failed. Exiting...")
+        return
+    extract_files()
+    execute_script()
+    print("The configure_node.sh script will now reboot the VM.")
+
+if __name__ == "__main__":
+    main()
